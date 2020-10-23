2e07ed97de704cdb855b1fe9139d2943ffa3b436
Luciano Silva <luciano2023silva@gmail.com>
2020-10-23 15:11:24 -0300

    add extension uuid-ossp

--- src/shared/infra/typeorm/migrations/1603471693991-CreateCustomers.ts
+++ src/shared/infra/typeorm/migrations/1603471693991-CreateCustomers.ts
@@ -3,6 +3,7 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 export default class CreateCustomers1603471693991
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
     await queryRunner.createTable(
       new Table({
         name: 'customers',
--- src/shared/infra/typeorm/migrations/1603471744827-CreateOrdersProducts.ts
+++ src/shared/infra/typeorm/migrations/1603471744827-CreateOrdersProducts.ts
@@ -30,7 +30,7 @@ export default class CreateOrdersProducts1603471744827
           },
           {
             name: 'quantity',
-            type: 'numeric',
+            type: 'integer',
           },
           {
             name: 'created_at',
